d7d02752ea842646c0348a3af82f3e0f18869278
Gianluca Amato <gianluca.amato.74@gmail.com>
2025-05-14 15:59:48 +0200

    Add raw results and some scripts for generating tables.

---
+++ results/counts_m3.py
@@ -0,0 +1,23 @@
+from math import log2
+import json
+import pandas as pd
+
+col_order = ["k", "n"]
+col_count = "count"
+col_val = "v"
+
+data = []
+with open("counts.json", "r") as f:
+    for line in f:
+        data.append(json.loads(line))
+
+df = pd.DataFrame(data)
+df = df[df["weights"].apply(lambda x: len(x) == 3 and x[0] == 0 and x[1] == 1)]
+df[col_val] = df["weights"].apply(lambda x: x[2])
+
+grouped = df.groupby(col_order + [col_count])
+grouped = grouped.agg({
+    col_val: lambda x: f"{x.min()} - {x.max()}" if x.max() < 128 else f"{x.min()} - 2**{int(log2(x.max()))}"
+}).reset_index()
+grouped = grouped[col_order + [col_val, col_count]]
+print(grouped.to_markdown(index=False))
---
+++ results/counts_p2.py
@@ -0,0 +1,35 @@
+from math import log2
+import json
+import pandas as pd
+
+col_order = ["k", "n"]
+col_count = "count"
+col_val = "val"
+
+data = []
+with open("counts.json", "r") as f:
+    for line in f:
+        data.append(json.loads(line))
+
+df_orig = pd.DataFrame(data)
+ks = df_orig["k"].unique()
+
+for k in ks:
+    df = df_orig[df_orig["k"] == k]
+    df = df[df["weights"].apply(lambda x: x == [2**i for i in range(len(x))])]
+    df["m"] = df["weights"].apply(lambda x: len(x)-1)
+    pivot = df.pivot_table(index="m", columns="n", values="count", fill_value=-1).astype(int).astype(str)
+    pivot = pivot.replace("-1", "")
+
+    print("\n\nk =", k)
+    print(pivot.to_markdown())
+
+
+
+
+# grouped = df.groupby(col_order + [col_count])
+# grouped = grouped.agg({
+#     col_val: lambda x: f"{x.min()} - {x.max()}" if x.max() < 128 else f"{x.min()} - 2**{int(log2(x.max()))}"
+# }).reset_index()
+# grouped = grouped[col_order + [col_val, col_count]]
+# print(grouped.to_markdown(index=False))
