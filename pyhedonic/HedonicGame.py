Add computation of prices of anaarchy and stability. Fix bugs.

pyhedonic/HedonicGame.py
@@ -1,5 +1,4 @@
-from itertools import combinations
-from typing import Iterator
+from typing import Iterator, NamedTuple
 
 import networkx as nx
 import numpy as np
@@ -14,6 +13,18 @@ type IntArray2D = np.ndarray[tuple[int, int], np.dtype[np.integer]]
 type IntArray1D = np.ndarray[tuple[int], np.dtype[np.integer]]
 
 type Coalition = int
+
+
+class PriceResult(NamedTuple):
+    """A named tuple to store the price of anarchy, the price of stability and corresponding coalition structures."""
+    poa: float
+    """Price of anarchy"""
+    pos: float
+    """Price of stability"""
+    cs_worst: 'CoalitionStructure'
+    """Coalition structure with the worst price"""
+    cs_best: 'CoalitionStructure'
+    """Coalition structure with the best price"""
 
 
 class HedonicGame:
@@ -35,9 +46,11 @@ class HedonicGame:
 
     def __init__(self, valuations: IntArray2D, is_symmetric: bool = True):
         """
-        Creates an hedonic game. The parameter `is_symmetric` should be consistent with
-        the values in the `valuations` matrix. If `is symmetric` is `True`, then
-        `valuations[i, j]` should be equal to `valuations[j, i]` for all `i` and `j`.
+        Create an hedonic game.
+
+        The parameter `is_symmetric` should be consistent with the values in the `valuations` matrix. If
+        `is symmetric` is `True`, then `valuations[i, j]` should be equal to `valuations[j, i]`
+        for all `i` and `j`.
         """
         self.valuations = valuations
         self.is_symmetric = is_symmetric
@@ -50,26 +63,22 @@ class HedonicGame:
         return len(self.valuations)
 
     def __eq__(self, other: object) -> bool:
-        """
-        Compare two hedonic games. The comparison is based on the valuations matrix and
-        the symmetry property.
-        """
         if not isinstance(other, HedonicGame):
             return False
         return np.array_equal(self.valuations, other.valuations) and self.is_symmetric == other.is_symmetric
 
     def to_dot(self) -> pydot.Dot:
         """
-        Convert the graph in the dot format. At the moment only works for symmetric games.
+        Convert the game in the dot format.
         """
-        if not self.is_symmetric:
-            raise ValueError("The game is not symmetric, cannot generate dot format.")
-        graph = pydot.Dot("hedonicgame", graph_type="graph")
+        graph_type = "graph" if self.is_symmetric else "digraph"
+        graph = pydot.Dot("hedonicgame", graph_type=graph_type, strict=True)
         for i in range(self.agents_num):
-            node = pydot.Node(str(i), label=str(i))
+            node = pydot.Node(str(i))
             graph.add_node(node)
         for i in range(self.agents_num):
-            for j in range(i+1, self.agents_num):
+            base = i+1 if self.is_symmetric else 0
+            for j in range(base, self.agents_num):
                 if self.valuations[i, j] > 0:
                     edge = pydot.Edge(str(i), str(j), label=str(self.valuations[i, j]))
                     graph.add_edge(edge)
@@ -80,31 +89,36 @@ class HedonicGame:
         """
         Convert a networkx graph to an hedonic game.
         """
-        if not all(isinstance(weight, int) for _, _, weight in graph.edges(data='weight')):
+        is_symmetric = not graph.is_directed()
+        if not all(isinstance(weight, int) or weight is None for _, _, weight in graph.edges(data="weight")):
             raise ValueError("The weights of the edges are not integers.")
         valuations = np.zeros((len(graph.nodes), len(graph.nodes)), dtype=np.integer)
         for i, j, weight in graph.edges(data='weight'):
-            valuations[i, j] = weight
-            valuations[j, i] = weight
-        return HedonicGame(valuations, is_symmetric=not graph.is_directed())
+            valuations[i, j] = weight if weight is not None else 1
+            if is_symmetric:
+                valuations[j, i] = valuations[i, j]
+        return HedonicGame(valuations, is_symmetric=is_symmetric)
 
-    def to_nx_graph(self) -> nx.Graph:
+    def to_nx_graph(self) -> nx.Graph | nx.DiGraph:
         """
-        Convert the graph in the networkx format.
+        Convert the game in the networkx format. All the edges in the resulting graph have the weight attribute.
         """
         graph = nx.Graph() if self.is_symmetric else nx.DiGraph()
         graph.add_nodes_from(range(self.agents_num))
-        graph.add_edges_from(
-            (i, j, {'weight': weight})
-            for i, j in combinations(range(self.agents_num), 2)
+        graph.add_weighted_edges_from(
+            (i, j, weight)
+            for i in range(self.agents_num)
+            for j in range(self.agents_num)
             if (weight := self.valuations[i, j]) > 0
         )
         return graph
 
-    def coalition_structures(self, cs_size: int | None = None,  k: int | None = None) -> Iterator['CoalitionStructure']:
+    def coalition_structures(self, k: int | None = None, cs_size: int | None = None) -> Iterator['CoalitionStructure']:
         """
-        Iterates over the colation structures for the current game. If provided, `cs_size` is the number of
-        coalitions in the coalition structure, while `k` is the maximum size of each coalition.
+        Iterates over the coalition structures of the game.
+
+        If provided, `cs_size` is the number of coalitions in the coalition structure, while `k` is
+        the maximum size of each coalition.
         """
         if cs_size is not None:
             for cs in hgimpl.css_givensize(self.valuations, cs_size, k):
@@ -113,22 +127,84 @@ class HedonicGame:
             for cs in hgimpl.css(self.valuations, k):
                 yield CoalitionStructure(self, cs)
 
-    def nash_equilibria(self, is_fractional: bool = True, k: int | None = None) -> Iterator['CoalitionStructure']:
+    def nash_stable_coalition_structures(self, is_fractional: bool = True, k: int | None = None) -> Iterator['CoalitionStructure']:
         """
-        Iterates over the the Nash stable coalition structures of the game. If provided, `cs_size` is the number of
-        coalitions in the coalition structure, while `k` is the maximum size of each coalition.
+        Iterates over the the Nash stable coalition structures of the game.
+
+        The parameter `is_fractional` tells if we are interested in fractional or additively separable games,
+        while `k` is the maximum size of each coalition.
         """
         for cs in hgimpl.nash_equilibria(self.valuations, is_fractional, k):
             yield CoalitionStructure(self, cs)
 
-    def has_nash_equilibrium(self, is_fractional: bool = True, k: int | None = None) -> bool:
+    def has_nash_stable_coalition_structure(self, is_fractional: bool = True, k: int | None = None) -> bool:
         """
         Return whether the game as has a Nash stable coalition structure.
+
+        The parameter `is_fractional` tells if we are interested in fractional or additively separable games,
+        while `k` is the maximum size of each coalition.
         """
         return hgimpl.nash_equilibrium(self.valuations, is_fractional, k) is not None
 
+    def optimal_coalition_structure(self, is_fractional: bool = True, k: int | None = None) -> tuple['CoalitionStructure', int]:
+        """
+        Return one of the optimal coalition structures of the game and the corresponding social welfare.
+
+        The parameter `is_fractional` tells if we are interested in fractional or additively separable games,
+        while `k` is the maximum size of each coalition.
+        """
+        if not self.is_symmetric:
+            raise ValueError(
+                "The game is not symmetric, cannot compute optimal coalition structure."
+            )
+        if k != 2:
+            raise ValueError(
+                "k is different from 2, cannot compute the optimal coalition structure."
+            )
+        g = self.to_nx_graph()
+        matching = (
+            nx.maximal_matching(g)
+            if np.max(self.valuations) <= 1
+            else nx.max_weight_matching(g)
+        )
+        welfare = sum(g[u][v]['weight'] for u, v in matching)
+        if not is_fractional:
+            welfare *= 2
+        cs = np.zeros(self.agents_num, dtype=np.integer)
+        for n, (i, j) in enumerate(matching):
+            cs[i] = n
+            cs[j] = n
+        return CoalitionStructure(self, cs, is_fractional), welfare
+
+    def prices(self, is_fractional: bool = True, k: int | None = None) -> PriceResult | None:
+        """
+        Return the prics of anarchy and the price of stability for the game, together with an example of the
+        coalition structures that achieve them. If the game has no Nash stable coalition structure, the
+        result is `None`.
+
+        The parameter `is_fractional` tells if we are interested in fractional or additively separable games,
+        while `k` is the maximum size of each coalition.
+        """
+        poa = float('-inf')
+        cs_worst = None
+        pos = float('inf')
+        cs_best = None
+        _, opt = self.optimal_coalition_structure(is_fractional, k)
+        for cs in self.nash_stable_coalition_structures(is_fractional, k):
+            price = opt / cs.social_welfare()
+            if price > poa:
+                poa = price
+                cs_worst = cs
+            if price < pos:
+                pos = price
+                cs_best = cs
+        return None if cs_worst is None or cs_best is None else PriceResult(poa, pos, cs_worst, cs_best)
+
     def __repr__(self) -> str:
         return f"HedonicGame({repr(self.valuations)}, is_symmetric={self.is_symmetric})"
+
+    def __str__(self) -> str:
+        return f"{repr(self.valuations)}, is_symmetric={self.is_symmetric}"
 
 
 class CoalitionStructure:
@@ -141,9 +217,15 @@ class CoalitionStructure:
     The game for which the coalition structure is defined.
     """
 
+    size: int
+    """
+    The number of coalitions in the coalition structure.
+    """
+
     cs: IntArray1D
     """
-    The coalition structure. The i-th element is the coalition number of the i-th agent.
+    The coalition structure. The i-th element is the coalition number of the i-th agent. Elements of `cs` are
+    all and only the integers in the range [0, size-1].
     """
 
     is_fractional: bool
@@ -152,20 +234,16 @@ class CoalitionStructure:
     additively separable one.
     """
 
-    size: int
-    """
-    The number of coalitions in the coalition structure.
-    """
-
     def __init__(self, game: HedonicGame, cs: IntArray1D, is_fractional: bool = True):
         """
         Creates a coalition structure for a given game. The coalition structure is represented as a
         numpy array of integers, where the i-th element is the coalition number of the i-th agent.
+        Elements of `cs` are all and only the integers in the range [0, max(self.cs)].
         """
         self.game = game
+        self.size = max(cs)+1
         self.cs = cs
         self.is_fractional = is_fractional
-        self.size = max(self.cs)+1
 
     def coalition_size(self, co: Coalition) -> int:
         """
@@ -177,8 +255,6 @@ class CoalitionStructure:
         """
         Returns the coalition of the given agent.
         """
-        if ag >= self.game.agents_num:
-            raise ValueError(f"Agent {ag} is not in the game.")
         return self.cs[ag]
 
     def agent_utility(self, ag: Agent) -> int | float:
@@ -186,32 +262,20 @@ class CoalitionStructure:
         Returns the utility of the given agent.
         """
         val, size = hgimpl.agent_utility(self.game.valuations, self.cs, ag)
-        return val if self.is_fractional else val / size
+        return val / size if self.is_fractional else val
 
     def coalition_social_welfare(self, co: Coalition) -> int | float:
         """
         Returns the social welfare of the given coalition.
         """
         ut, size = hgimpl.coalition_social_welfare(self.game.valuations, self.cs, co)
-        if size == 0:
-            return 0
-        else:
-            return ut/size if self.is_fractional else ut
+        return ut / size if self.is_fractional else ut
 
     def social_welfare(self) -> int | float:
         """
         Returns the social welfare of the coalition structure.
         """
-        return sum(self.coalition_social_welfare(co) for co in np.arange(self.size))
-
-    def move_agent(self, ag: Agent, co_new: Coalition):
-        """
-        Move the given agent to the new coalition.
-        """
-        if co_new >= self.size:
-            co_new = self.size
-            self.size += 1
-        self.cs[ag] = co_new
+        return sum(self.coalition_social_welfare(co) for co in range(self.size))
 
     def is_improving_deviation(self, ag: Agent, co_new: Coalition) -> bool:
         """
@@ -230,8 +294,7 @@ class CoalitionStructure:
         else:
             return ut_new * size_old > ut_old * (size_new + 1)
 
-
-    def is_agent_stable(self, ag: Agent, k: np.integer | None = None) -> bool:
+    def is_agent_nash_stable(self, ag: Agent, k: np.integer | None = None) -> bool:
         """
         Determine if the given agent has no improving deviations.
         """
@@ -244,10 +307,13 @@ class CoalitionStructure:
         """
         Determine if the coalition structure is Nash stable.
         """
-        return all(self.is_agent_stable(ag, k) for ag in range(self.game.agents_num))
+        return all(self.is_agent_nash_stable(ag, k) for ag in range(self.game.agents_num))
 
     def __repr__(self) -> str:
         return f"CoalitionStructure({repr(self.game)},{repr(self.cs)})"
+
+    def __str__(self) -> str:
+        return f"{repr(self.cs)}"
 
 
 GAME_K3_NOEQUILIBRIUM_PAPER = HedonicGame(np.array([

tests/test_pyhedonic.py
@@ -1,7 +1,14 @@
-import pyhedonic.HedonicGame as hg
+"""
+This file contains unit tests for the pyhedonic library.
 
-import numpy as np
+The notation [PAPER] refers to the paper "Nash Stability in Fractional Hedonic Games
+with Bounded Size Coalitions".
+"""
 import networkx as nx
+import numpy as np
+import pytest
+
+import pyhedonic.HedonicGame as hg
 
 valuations1 = np.array([
     [0, 0, 2],
@@ -42,11 +49,11 @@ def test_coalition_size():
 
 
 def test_agent_utility():
-    assert cs11.agent_utility(0) == 2
-    assert cs11.agent_utility(1) == 0
-    assert cs11.agent_utility(2) == 2
-    assert cs21.agent_utility(0) == 1.0
-    assert cs21.agent_utility(1) == 0.0
+    assert cs11.agent_utility(0) == 1.0
+    assert cs11.agent_utility(1) == 0.0
+    assert cs11.agent_utility(2) == 1.0
+    assert cs21.agent_utility(0) == 2
+    assert cs21.agent_utility(1) == 0
 
 
 def test_coalition_social_welfare():
@@ -94,17 +101,17 @@ def test_coalition_structures3():
 
 
 def test_Nash_stability():
-    compare_coalition_structures(game1.nash_equilibria(), [[0, 0, 0]])
-    compare_coalition_structures(game1.nash_equilibria(k=1), [[0, 1, 2]])
+    compare_coalition_structures(game1.nash_stable_coalition_structures(), [[0, 0, 0]])
+    compare_coalition_structures(game1.nash_stable_coalition_structures(k=1), [[0, 1, 2]])
 
 
 def test_has_nash_equilibrium():
-    assert not hg.GAME_K3_NOEQUILIBRIUM_PAPER.has_nash_equilibrium(k=3)
-    assert not hg.GAME_K3_NOEQUILIBRIUM.has_nash_equilibrium(k=3)
-    assert not hg.GAME_K4_NOEQUILIBRIUM.has_nash_equilibrium(k=4)
-    assert not hg.GAME_K5_NOEQUILIBRIUM.has_nash_equilibrium(k=5)
-    assert not hg.GAME_K6_NOEQUILIBRIUM.has_nash_equilibrium(k=6)
-    assert not hg.GAME_K7_NOEQUILIBRIUM.has_nash_equilibrium(k=7)
+    assert not hg.GAME_K3_NOEQUILIBRIUM_PAPER.has_nash_stable_coalition_structure(k=3)
+    assert not hg.GAME_K3_NOEQUILIBRIUM.has_nash_stable_coalition_structure(k=3)
+    assert not hg.GAME_K4_NOEQUILIBRIUM.has_nash_stable_coalition_structure(k=4)
+    assert not hg.GAME_K5_NOEQUILIBRIUM.has_nash_stable_coalition_structure(k=5)
+    assert not hg.GAME_K6_NOEQUILIBRIUM.has_nash_stable_coalition_structure(k=6)
+    assert not hg.GAME_K7_NOEQUILIBRIUM.has_nash_stable_coalition_structure(k=7)
 
 
 def test_to_nx_graph():
@@ -131,3 +138,53 @@ def test_from_nx_graph():
         (2, 3, {'weight': 7})
     ])
     assert hg.HedonicGame.from_nx_graph(graph) == hg.GAME_K3_NOEQUILIBRIUM_PAPER
+
+def test_optimal_coalition_structure():
+    game = hg.HedonicGame(np.array([
+        [0, 1, 0, 1, 0],
+        [1, 0, 1, 1, 0],
+        [0, 1, 0, 0, 1],
+        [1, 1, 0, 0, 1],
+        [0, 0, 1, 1, 0]
+    ]))
+    _, v = game.optimal_coalition_structure(k=2)
+    assert v == 2
+
+@pytest.mark.parametrize("k", [2, 3])
+def test_no_nash_for_asymmetric_games(k: int):
+    """
+    Test inspired by Proposition 1 in [PAPER].
+    """
+    g = nx.DiGraph()
+    for i in range(k+1):
+        g.add_edge(i, (i + 1) % (k+1))
+    graph = hg.HedonicGame.from_nx_graph(g)
+    assert not graph.has_nash_stable_coalition_structure(k=k)
+
+@pytest.mark.parametrize("m", [10, 20])
+def test_unbound_poa_for_non_simple_games(m: int):
+    """
+    Test inspired by Proposition 2 in [PAPER].
+    """
+    g = nx.Graph()
+    g.add_edge(0, 1, weight=1)
+    g.add_edge(1, 2, weight=2*m)
+    g.add_edge(2, 3, weight=1)
+    graph = hg.HedonicGame.from_nx_graph(g)
+    prices = graph.prices(k=2)
+    cs, opt = graph.optimal_coalition_structure(k=2)
+    assert opt == 2*m
+    assert prices is not None
+    assert prices.poa == m
+
+def test_prices_for_2SSFHG():
+    """
+    Test inspired by Proposition 4 in [PAPER].
+    """
+    g = nx.Graph()
+    g.add_edges_from([(0, 1), (1,2), (2,3)])
+    game = hg.HedonicGame.from_nx_graph(g)
+    pr = game.prices(k=2)
+    assert pr is not None
+    assert pr.poa == 2.0
+    assert pr.pos == 1.0