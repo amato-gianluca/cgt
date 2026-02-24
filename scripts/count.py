b56f422fa790be92194598e4edc6bdebd99dd314
Gianluca Amato <gianluca.amato.74@gmail.com>
2026-02-24 14:58:56 +0100

    Generalize the count.py script.

--- scripts/count.py
+++ scripts/count.py
@@ -1,27 +1,90 @@
 """
-This script counts the number of games with no stable coalition structures.
-You can pass the maximum size of coalitions `k` as a command line argument.
-The script will iterate over the number of agents and the maximum valuation,
-trying to keep them low enough that the computation is actually feasible.
+This script counts the number of games with no Nash stable coalition structures.
 """
 
-from sys import argv
+import argparse
+import datetime
+import json
+import sys
+import time
+from typing import Collection
+
 from pyhedonic.hedonicgame_impl import *
 
-k = int(argv[1])
 
-n_min = int(argv[2]) if len(argv) >= 3 else k+1
-n_max = 11
+def result_log(k: int, n: int, m: int, weights: IntArray1D | None,  total_game_count: int,
+               unstable_game_count: int, elapsed_time: float, example: IntArray2D | None) -> str:
+    data = {
+        "k": k,
+        "n": n,
+        "m": m,
+        "weights": weights,
+        "total_game_count": total_game_count,
+        "unstable_game_count": unstable_game_count,
+        "elapsed_time": elapsed_time,
+        "elapsed_time_human": str(datetime.timedelta(seconds=int(elapsed_time))),
+        "example": example.tolist() if example is not None else None,
+    }
+    return json.dumps(data)
 
-m_min = int(argv[3]) if len(argv) >= 4 else 0
-m_max_k0 = [20, 20, 20, 20, 20, 10, 7, 3, 2, 1, 1]
-m_max_k4 = [20, 20, 20, 20, 20, 10, 6, 3, 2, 1, 1]
-m_max = m_max_k0 if k <= 3 else m_max_k4
 
-for n in range(n_min, n_max+1):
-    for m in range(m_min, m_max[n]+1):
-        m_min = 0
-        print("num_agents:", n, "k:", k, "maxval: ", m)
-        count_noequilibrium, count_total = count_unstable_games(
-            agent_count=n, k=k, m_begin=m, m_end=m, debug=1)
-        print("num_agents:", n, "k:", k, "maxval: ", m, "count:", count_noequilibrium, "/", count_total)
+def parse_range(s: str) -> Collection[int]:
+    s_split = s.split("-")
+    val_min = int(s_split[0])
+    val_max = int(s_split[1]) if len(s_split) > 1 else val_min
+    return range(val_min, val_max+1)
+
+
+parser = argparse.ArgumentParser(
+    prog='count.py',
+    description='''
+    Count games without Nash table coalition structures.
+    Arguments n, k and m may be either a natural number or a range min-max of natural numbers.
+    ''')
+parser.add_argument('k', nargs='?', help='upper bound on the size of coalitions')
+parser.add_argument('n', nargs='?', help='number of agents in the game')
+parser.add_argument('m', nargs='?', help='maximum valuation in the game')
+parser.add_argument('-o', '--output', help='output file')
+parser.add_argument('-w', '--weights', help='weights to use instead of consecutive numbers')
+args = parser.parse_args()
+
+n_range = parse_range(args.n)
+k_range = parse_range(args.k)
+m_range = parse_range(args.m)
+
+f = open(args.output, "a") if args.output else None
+weights = json.loads(args.weights) if args.weights else None
+
+# warmup jit
+count_unstable_games(agent_count=1, m_begin=1, m_end=1, debug=0)
+
+if k_range is None:
+    k_range = range(3, 8)
+
+for k in k_range:
+    if n_range is None:
+        n_range = range(k+1, 11)
+
+    for n in n_range:
+        if m_range is None:
+            match n:
+                case 0 | 1 | 2 | 3 | 4: m_range = range(1, 31)
+                case 5: m_range = range(1, 11)
+                case 6: m_range = range(1, 7)
+                case 7: m_range = range(1, 4)
+                case 8: m_range = range(1, 3)
+                case _: m_range = range(1, 2)
+
+        for m in m_range:
+            print("k:", k, "n:", n, "m: ", m, file=sys.stderr)
+            start_time = time.time()
+            unstable_game_count, total_game_count, example = count_unstable_games(
+                agent_count=n, k=k, m_begin=m, m_end=m, weights=weights, debug=1)
+            elapsed_time = time.time() - start_time
+            log_msg = result_log(n, k, m, weights, total_game_count, unstable_game_count, elapsed_time, example)
+            print(log_msg)
+            if f:
+                print(log_msg, file=f)
+
+if f:
+    f.close()
