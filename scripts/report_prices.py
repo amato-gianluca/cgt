309028d015c603e801af18f1ab038e08e74263ca
Gianluca Amato <gianluca.amato.74@gmail.com>
2026-05-06 15:36:34 +0200

    Remove lowest prices from documented results.

--- scripts/report_prices.py
+++ scripts/report_prices.py
@@ -51,7 +51,7 @@ def main():
     df = df.dropna(subset= ["payload"])
     columns = ["k", "n", "m"]
     for price in ["poa", "pos"]:
-        for value in ["highest", "lowest"]:
+        for value in ["highest"]:  # add "lowest" later if needed
             columns += [f"{price}_{value}", f"{price}_{value}_count"]
             df[f"{price}_{value}"] = df["payload"].map(lambda x: x[f"{price}_{value}"][f"{price}"]["numerator"] / x[f"{price}_{value}"][f"{price}"]["denominator"])
             df[f"{price}_{value}_count"] = df["payload"].map(lambda x: x[f"{price}_{value}_count"]).astype(str)
