77c0aa057d4dce8e9acde74a0d5856c5820e2715
Gianluca Amato <gianluca.amato.74@gmail.com>
2026-05-31 15:46:41 +0200

    Update reports_prices.py to work with the new unified YAML format.

--- scripts/report_prices.py
+++ scripts/report_prices.py
@@ -12,11 +12,33 @@ from typing import Any
 import pandas as pd
 import yaml
 
+type GCILike = dict[str, Any]
+type InputData = list[GCILike]
 
-def yaml_load(filename: str) -> list[dict[str, Any]]:
+
+def yaml_load(filename: str) -> InputData:
+    """
+    Loads the data from a yaml file, which is expected to contain a list of documents, each
+    being a dictionary with informations serialized from a `GameCollectionInfo` object.
+    """
     with open(filename, "r") as f:
         data = list(yaml.load_all(f, Loader=yaml.FullLoader))
     return data
+
+
+def generate_df(data: InputData) -> pd.DataFrame:
+    """
+    Generates a pandas DataFrame from the list of dictionaries, filtering the rows based on the
+    compatibility of their weights with the specified weights.
+    """
+    data_clean = [
+        row
+        for row in data
+        if row["payload"] is not None and row["payload"].get("prices") is not None
+    ]
+    df = pd.json_normalize(data_clean)
+    return df
+
 
 def main():
 
@@ -32,29 +54,28 @@ def main():
         help="file containing data points to report about",
         default=Path(__file__).parent / "data/prices.yaml",
     )
-    parser.add_argument(
-        "-k",
-        help="specify the required valued of k to report about"
-    )
-    parser.add_argument(
-        "-m",
-        help="specify the required valued of m to report about"
-    )
-    parser.add_argument(
-        "-n",
-        help="specify the required valued of n to report about"
-    )
+    parser.add_argument("-k", help="specify the required valued of k to report about")
+    parser.add_argument("-m", help="specify the required valued of m to report about")
+    parser.add_argument("-n", help="specify the required valued of n to report about")
     args = parser.parse_args()
 
     data = yaml_load(args.input)
-    df = pd.DataFrame(data)
-    df = df.dropna(subset= ["payload"])
+    df = generate_df(data)
+    if df.empty:
+        print("No data to report about.")
+        return
+
     columns = ["k", "n", "m"]
     for price in ["poa", "pos"]:
         for value in ["highest"]:
             columns += [f"{price}_{value}", f"{price}_{value}_count"]
-            df[f"{price}_{value}"] = df["payload"].map(lambda x: x[f"{price}_{value}"]["numerator"] / x[f"{price}_{value}"]["denominator"])
-            df[f"{price}_{value}_count"] = df["payload"].map(lambda x: x[f"{price}_{value}_count"]).astype(str)
+            df[f"{price}_{value}"] = (
+                df[f"payload.prices.{price}_{value}.numerator"]
+                / df[f"payload.prices.{price}_{value}.denominator"]
+            )
+            df[f"{price}_{value}_count"] = df[
+                f"payload.prices.{price}_{value}_count"
+            ].astype(str)
     prices = df[columns].copy()
     if args.k is not None:
         prices = prices[prices["k"] == int(args.k)]
@@ -65,7 +86,8 @@ def main():
     if args.n is not None:
         prices = prices[prices["n"] == int(args.n)]
         prices.drop(columns=["n"], inplace=True)
-    print(prices.to_markdown(index=False, floatfmt='.2f'))
+    print(prices.to_markdown(index=False, floatfmt=".2f"))
+
 
 if __name__ == "__main__":
     main()
